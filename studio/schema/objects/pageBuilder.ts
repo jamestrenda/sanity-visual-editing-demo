ac38fabca8cfe390a0c14566aa7be213036dae31
James Trenda <jamestrenda@gmail.com>
2024-03-23 18:23:23 -0400

    fix ts issue

--- studio/schema/objects/pageBuilder.ts
+++ studio/schema/objects/pageBuilder.ts
@@ -1,39 +1,13 @@
-import { defineField, defineType } from 'sanity'
-import { IconSections } from '~/icons/sections'
+import { defineType } from 'sanity'
+import { blockContentTypes } from './blockContent'
+import PageBuilderInput from '~/components/PageBuilderInput'
 
 export default defineType({
   title: 'Page Builder',
   name: 'pageBuilder',
-  type: 'blockContent',
-  // of: [
-  //   {
-  //     type: 'object',
-  //     title: 'Container',
-  //     name: 'section',
-  //     preview: {
-  //       select: {
-  //         block: 'blocks[0].title',
-  //         title: 'title',
-  //       },
-  //       prepare({ title, block }) {
-  //         return {
-  //           title: title || block || 'Section',
-  //           media: IconSections,
-  //         }
-  //       },
-  //     },
-  //     fields: [
-  //       defineField({
-  //         title: ' ',
-  //         name: 'blocks',
-  //         type: 'blockContent',
-  //         // type: 'array',
-  //         // of: [{ type: 'blockContent' }],
-  //       }),
-  //     ],
-  //   },
-  // ],
-  // components: {
-  //   input: PageBuilderInput,
-  // },
+  type: 'array',
+  of: blockContentTypes,
+  components: {
+    input: PageBuilderInput,
+  },
 })
