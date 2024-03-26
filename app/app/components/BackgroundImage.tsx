342cc91be32c196ea06e26b2ff8acf2456b9ede5
James Trenda <jamestrenda@gmail.com>
2024-03-26 10:43:21 -0400

    add classNames to props

--- app/app/components/BackgroundImage.tsx
+++ app/app/components/BackgroundImage.tsx
@@ -1,13 +1,31 @@
-import Image, { ImageProps } from './Image'
+import { cn } from '~/utils/misc'
+import Image from './Image'
+import { SanityImageObjectExtended } from '~/types/image'
 
-const BackgroundImage = ({ source }: ImageProps) =>
+const BackgroundImage = ({
+  source,
+  wrapperClassNames,
+  classNames,
+}: {
+  source: SanityImageObjectExtended
+  wrapperClassNames?: string
+  classNames?: string
+}) =>
   source.asset ? (
-    <div className="absolute inset-0 -z-10 [&>img]:transition-opacity [&>img]:duration-500 pointer-events-none">
+    <div
+      className={cn(
+        `absolute inset-0 -z-10 [&>img]:transition-opacity [&>img]:duration-500 pointer-events-none`,
+        wrapperClassNames,
+      )}
+    >
       <Image
         source={source}
         width={1920}
         loading="eager"
-        className="absolute inset-0 h-full w-full object-cover opacity-10 transition duration-1000"
+        className={cn(
+          `absolute inset-0 h-full w-full object-cover opacity-10 transition duration-1000`,
+          classNames,
+        )}
         q={70}
       />
     </div>
